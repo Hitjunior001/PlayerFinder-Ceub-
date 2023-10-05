78f72cf26405cee6b2f102da66a68ec08b5c29b7
e008278 <gustavo.passos@tst.jus.br>
2023-10-05 14:35:09 -0300

    adicionando arquitetura

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='div-logo' >
+          <img src="logo.png" className="App-logo" alt="logo" />
+        </div>
+        <div className='div-tittle'>
+          <h1 className='tittle'>
+            Busca de Candidatos
+          </h1>
+        </div>
+        <div className='div-link'>
+          <a href='' className='cadastra-cand'>
+            Cadastrar candidato
+          </a>
+        </div>
+      </header>
+      <div className='div-nav-bar'>
+        <nav className='nav-bar'>
+        
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default App;
