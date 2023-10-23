f55666d92565a434babb329c4c6c216646c20ab7
Gustavo-Ehardt <gustavoespassos@sempreceub.com>
2023-10-23 20:32:55 -0300

    Header Css fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,15 @@ export const CustomAppBar = () => {
  return (
     <AppBar position="static">
       <Toolbar style={{height:"13vh", backgroundColor:"#202020"}}>
-        <img src="logo.png" alt="logo"/>
+        <a href=''>
+          <img src="logo.png" alt="logo"/>
+        </a>
         <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
           Busca de Candidatos
         </Typography>
-        <a href='' style={{color:"white"}}><Button color="inherit" className='cadastra-cand' style={{ textDecoration:"underline"}}>Cadastrar candidato</Button></a>
+        <a href='' style={{color:"white" }}>
+          <Button color="inherit" className='cadastra-cand' sx={{ textDecoration:"underline",  "&:hover":{color:"#29AFBD", textDecoration:"underline"} }}>Cadastrar candidato</Button>
+        </a>
       </Toolbar>
     </AppBar>
  )
