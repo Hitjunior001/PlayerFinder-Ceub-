202829f550fde4c12897327a477a166035141cef
e008278 <gustavo.passos@tst.jus.br>
2023-10-24 13:33:23 -0300

    Css Changes

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -17,33 +17,55 @@ export const NavBar = () => {
         <nav className='nav-bar'>
             <div className='busca'>
                 <h2 className="busca-tittle"  style={{marginBottom:"3vh", marginTop:"0vh"}}>Busca Principal</h2>
+                
                 <div className="busca-search" style={{marginBottom:"2vh"}}>
                     <SearchBar/>
                 </div>
+                
                 <div className="busca-radio" style={{marginBottom:"2vh"}}>
                     <RadioButtonsCA/>
                 </div>
-                <div className="busca-estado" style={{marginBottom:"3vh"}}>
+                
+                <div className="busca-estado" style={{marginBottom:"2vh"}}>
                     <CheckboxLabelsEstados/>
                 </div>
-                <div className="busca-cidade" style={{marginBottom:"1vh"}}>
+                
+                <div className="busca-cidade">
                     <Cidades/>
                 </div>
             </div>
             <div className='mais-filtros'>
-                <h3 className="tittle-nav-bar">Mais filtros</h3>
-                <div className="filtros-dropdown-1">
+                <h3 className="tittle-nav-bar"  style={{marginBottom:"3vh", marginTop:"0vh"}}>Mais filtros</h3>
+                
+                <div className="filtro-tech" style={{marginBottom:"2vh"}}>
                     <CheckboxLabelsTech/>
+                </div>
+                
+                <div className="filtro-Exp" style={{marginBottom:"2vh"}}>
                     <CheckboxLabelsExp/>
+                </div>
+                
+                <div className="filtro-Atuacao" style={{marginBottom:"2vh"}}>
                     <CheckboxLabelsAtuacao/>
                 </div>
-                <div className="filtros-radio">
+                
+                <div className="filtro-Cnha" style={{marginBottom:"2vh"}}>
                     <RadioButtonsCnhA/>
+                </div>
+
+                <div className="filtro-Cnha" style={{marginBottom:"2vh"}}>
                     <RadioButtonsTS/>
                 </div>
-                <div className="filtros-dropdown-2">
+                
+                <div className="filtro-D" style={{marginBottom:"2vh"}}>
                     <CheckboxLabelsD/>
+                </div>
+                
+                <div className="filtro-Horario" style={{marginBottom:"2vh"}}>
                     <CheckboxLabelsHorario/>
+                </div>
+
+                <div className="filtro-DH">
                     <CheckboxLabelsDH/>
                 </div>
             </div>
