0fc86ec9a1439e0970e51693f21d44810bd093f6
Gustavo-Ehardt <gustavoespassos@sempreceub.com>
2023-10-25 20:43:01 -0300

    Lista de Resultados - Itens CSS

--- src/components/results.js
+++ src/components/results.js
@@ -1,100 +1,78 @@
 import React from "react";
 import { Container, List, ListItem, Avatar, Typography } from "@mui/material";
 
-export const ResultList = () => {
-  const profiles = [
+// Extract profile information from the image
+const profiles = [
     {
-      name: "John Doe",
+      name: "Gustavo Erhardt dos Santos Passos",
       avatar: "https://avatars.githubusercontent.com/u/123456789?v=4",
-      email: "johndoe@example.com",
-      location: "São Paulo, Brasil",
-    },
-    {
-      name: "Jane Doe",
-      avatar: "https://avatars.githubusercontent.com/u/987654321?v=4",
-      email: "janedoe@example.com",
-      location: "Rio de Janeiro, Brasil",
-    },
-    {
-      name: "John Doe",
-      avatar: "https://avatars.githubusercontent.com/u/123456789?v=4",
-      email: "johndoe@example.com",
-      location: "São Paulo, Brasil",
-    },
-    {
-      name: "Jane Doe",
-      avatar: "https://avatars.githubusercontent.com/u/987654321?v=4",
-      email: "janedoe@example.com",
-      location: "Rio de Janeiro, Brasil",
-    },
-    {
-      name: "John Doe",
-      avatar: "https://avatars.githubusercontent.com/u/123456789?v=4",
-      email: "johndoe@example.com",
-      location: "São Paulo, Brasil",
-    },
-    {
-      name: "Jane Doe",
-      avatar: "https://avatars.githubusercontent.com/u/987654321?v=4",
-      email: "janedoe@example.com",
-      location: "Rio de Janeiro, Brasil",
-    },
-    {
-      name: "John Doe",
-      avatar: "https://avatars.githubusercontent.com/u/123456789?v=4",
-      email: "johndoe@example.com",
-      location: "São Paulo, Brasil",
-    },
-    {
-      name: "Jane Doe",
-      avatar: "https://avatars.githubusercontent.com/u/987654321?v=4",
-      email: "janedoe@example.com",
-      location: "Rio de Janeiro, Brasil",
-    },
-    {
-      name: "John Doe",
-      avatar: "https://avatars.githubusercontent.com/u/123456789?v=4",
-      email: "johndoe@example.com",
-      location: "São Paulo, Brasil",
-    },
-    {
-      name: "Jane Doe",
-      avatar: "https://avatars.githubusercontent.com/u/987654321?v=4",
-      email: "janedoe@example.com",
-      location: "Rio de Janeiro, Brasil",
-    },
-    {
-      name: "John Doe",
-      avatar: "https://avatars.githubusercontent.com/u/123456789?v=4",
-      email: "johndoe@example.com",
-      location: "São Paulo, Brasil",
-    },
-    {
-      name: "Jane Doe",
-      avatar: "https://avatars.githubusercontent.com/u/987654321?v=4",
-      email: "janedoe@example.com",
-      location: "Rio de Janeiro, Brasil",
+      email: "gustavo.passos@example.com",
+      location: "Brasília, DF",
+      joinDate: "2023-09-25",
+      age: 20,
+      active: true,
+      experience: "Júnior",
+      workMode: "Remoto",
+      primarySkill: "Java",
+      secondarySkills: ["React"],
     },
   ];
 
+export const ResultList = () => {
   return (
     <div className="results-container">
       <Container style={{maxWidth:"75vw"}}>
-        <h2 style={{ marginBottom:"1vh", marginTop:"0vh" }}>Resultados</h2>
-        <List style={{display:"flex", flexDirection:"column", alignItems:"center", height:"75vh", overflowY:"scroll", borderTop:"1px solid #9E9E9E", borderBottom:"1px solid #9E9E9E"}}>
+        <h2 style={{ marginBottom:"1vh", marginTop:"0vh" }}>Candidatos Filtrados</h2>
+
+        <List style={{display:"flex", flexDirection:"column", alignItems:"center", maxHeight:"75vh", overflowY:"scroll", borderTop:"1px solid #9E9E9E", borderBottom:"1px solid #9E9E9E"}}>
           {profiles.map((profile) => (
+            
             <ListItem key={profile.name} style={{ marginBottom:"1vh", marginTop:"1vh", width:"98%", height:"15vh", display:"flex", flexDirection:"row"}}>
-              <Avatar src={profile.avatar} style={{ height:"75%", width:"auto", marginLeft:"1%", marginRight:"2%" }} />
-              <div style={{ display:"flex", flexDirection:"column" }}>
-                <Typography variant="h6">{profile.name}</Typography>
+            <Avatar src={profile.avatar} style={{ height:"75%", width:"auto", marginLeft:"1%", marginRight:"2%" }} />
+            <div style={{ display:"flex", flexDirection:"column" }}>
+              
+              <div>  
+                <Typography variant="h6">
+                  {profile.name}
+                </Typography>
+              </div>
+              
+              <div style={{display:"flex", flexDirection:"row"}}>
                 <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
                   {profile.email}
                 </Typography>
                 <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
                   {profile.location}
                 </Typography>
+                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                  {profile.joinDate}
+                </Typography>
+                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                  {profile.age}
+                </Typography>
+                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                  {profile.active ? "Ativo" : "Inativo"}
+                </Typography>
+                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                  {profile.experience}
+                </Typography>
+                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                  {profile.workMode}
+                </Typography>
               </div>
-            </ListItem>
+
+              <div style={{display:"flex", flexDirection:"row"}}>  
+                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                  {profile.primarySkill}
+                </Typography>
+                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                  {profile.secondarySkills.join(", ")}
+                </Typography>
+              </div>
+
+            </div>
+          </ListItem>
+          
           ))}
         </List>
       </Container>
