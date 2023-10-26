a5189531f447351aa5a56dff1b6d78c572b18bb2
e008278 <gustavo.passos@tst.jus.br>
2023-10-26 13:15:56 -0300

    Result list finished

--- src/components/results.js
+++ src/components/results.js
@@ -1,22 +1,55 @@
 import React from "react";
 import { Container, List, ListItem, Avatar, Typography } from "@mui/material";
 
-// Extract profile information from the image
 const profiles = [
     {
+      avatar: "https://avatars.githubusercontent.com/u/987654321?v=4",
       name: "Gustavo Erhardt dos Santos Passos",
-      avatar: "https://avatars.githubusercontent.com/u/123456789?v=4",
-      email: "gustavo.passos@example.com",
-      location: "Brasília, DF",
-      joinDate: "2023-09-25",
       age: 20,
+      email: "gustavoespassos@gmail.com",
+      location: "Brasília, DF",
+      joinDate: "25/09/2023",
       active: true,
       experience: "Júnior",
       workMode: "Remoto",
       primarySkill: "Java",
-      secondarySkills: ["React"],
+      secondarySkills: ["React", "Python"],
+    },
+    {
+      avatar: "https://avatars.githubusercontent.com/u/123456789?v=4",
+      name: "Reginaldo de Oliveira Júnior",
+      age: 21,
+      email: "juninhorf08@sempreceub.com",
+      location: "Brasília, DF",
+      joinDate: "24/08/2023",
+      active: false,
+      experience: "Pleno",
+      workMode: "Presencial",
+      primarySkill: "Java",
+      secondarySkills: ["React", "C#"],
     },
   ];
+
+  const TechStyle = ({ tech }) => {
+    return (
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "fit-content",
+        height:"fit-content",
+        maxHeight:"1.5vh",
+        padding: "0.5rem",
+        borderRadius: "0.5rem",
+        background: "#c4c4c4",
+        marginRight: "0.5vw",
+      }}>
+        <Typography variant="subtitle1" style={{ color: "#696969" }}>
+          {tech}
+        </Typography>
+      </div>
+    );
+  };
 
 export const ResultList = () => {
   return (
@@ -31,43 +64,45 @@ export const ResultList = () => {
             <Avatar src={profile.avatar} style={{ height:"75%", width:"auto", marginLeft:"1%", marginRight:"2%" }} />
             <div style={{ display:"flex", flexDirection:"column" }}>
               
-              <div>  
+              <div style={{display:"flex", flexDirection:"row"}}>  
+                <Typography variant="h6" style={{marginRight:" 0.5vw"}}>
+                  {profile.name},
+                </Typography>
                 <Typography variant="h6">
-                  {profile.name}
+                  {profile.age}
                 </Typography>
               </div>
               
-              <div style={{display:"flex", flexDirection:"row"}}>
-                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
-                  {profile.email}
+              <div style={{display:"flex", flexDirection:"row", color:"#9E9E9E", marginBottom:"1vh"}}>
+                <Typography variant="subtitle1" style={{marginRight:"1.5vw" }}>
+                  {profile.email} 
                 </Typography>
-                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                <Typography variant="subtitle1" style={{marginRight:"1.5vw" }}>
                   {profile.location}
                 </Typography>
-                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                <Typography variant="subtitle1" style={{marginRight:"1.5vw" }}>
                   {profile.joinDate}
                 </Typography>
-                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
-                  {profile.age}
-                </Typography>
-                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                <Typography variant="subtitle1" style={{marginRight:"1.5vw" }}>
                   {profile.active ? "Ativo" : "Inativo"}
                 </Typography>
-                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                <Typography variant="subtitle1" style={{marginRight:"1.5vw" }}>
                   {profile.experience}
                 </Typography>
-                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
+                <Typography variant="subtitle1" style={{marginRight:"1.5vw" }}>
                   {profile.workMode}
                 </Typography>
               </div>
 
-              <div style={{display:"flex", flexDirection:"row"}}>  
-                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
-                  {profile.primarySkill}
-                </Typography>
-                <Typography variant="subtitle1" style={{ color:"#9E9E9E" }}>
-                  {profile.secondarySkills.join(", ")}
-                </Typography>
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                <Typography variant="subtitle1" style={{marginRight:"0.5vw", textDecoration:"underline", color:"#696969"}}>Principal: </Typography>
+                <TechStyle tech={profile.primarySkill} />
+                <Typography variant="subtitle1" style={{marginLeft:"2vw", marginRight:"0.5vw", textDecoration:"underline", color:"#696969"}}>Secundárias: </Typography>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  {profile.secondarySkills.map((skill) => (
+                    <TechStyle tech={skill} key={skill} />
+                  ))}
+                </div>
               </div>
 
             </div>
