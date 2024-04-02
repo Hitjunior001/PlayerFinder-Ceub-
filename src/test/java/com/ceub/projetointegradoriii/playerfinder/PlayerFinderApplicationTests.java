d866b745c059328b5375bbaf2b1defd4b59c3d48
Digs-LS <diego.lucas.sousa@live.com>
2024-04-02 19:16:18 -0300

    feat: setup e entidade 'usuario'

---
+++ src/test/java/com/ceub/projetointegradoriii/playerfinder/PlayerFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceub.projetointegradoriii.playerfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
