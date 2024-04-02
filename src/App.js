578837c5553933e0ad7dbff9f09ccbc631601f0a
Gustavo_Erhardt <119618665+GustavoErhardt@users.noreply.github.com>
2024-04-02 17:31:12 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Routes from "./Routes";s
+import Routes from "./Routes";
 
 import { CustomAppBar } from "./components/Header";
 
