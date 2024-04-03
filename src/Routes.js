cfa3add924cae112e94a418ae65318499d431e90
E008278 <gustavoespassos@tst.jus.br>
2024-04-03 11:43:16 -0300

    React-Router-Dom

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
 
+// eslint-disable-next-line
 export default () => {
     return (
-        <Switch>
-            <Route exact path="/cadastro">
-                <Cadastro/>
-            </Route>                
-            <Route exact path="/login">
-                <Login/>
-            </Route>                
-        </Switch>
+        // <Switch>
+        //     <Route exact path="/cadastro">
+        //         <Cadastro/>
+        //     </Route>                
+        //     <Route exact path="/login">
+        //         <Login/>
+        //     </Route>                
+        // </Switch>
+        <Router>
+            <Routes>
+                <Route path="/cadastro" element={<Cadastro />}  />
+                <Route path="/login" element={<Login />}  />
+            </Routes>
+        </Router>
     );
 }
