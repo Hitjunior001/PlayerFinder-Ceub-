53ef5c02b1af27a1d936b53f51e003b83ec899f3
E008278 <gustavoespassos@tst.jus.br>
2024-04-11 14:30:42 -0300

    [ Inicio feats ]

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -7,6 +7,7 @@ import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import HomeIcon from '@mui/icons-material/Home';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import JogosAreaCard from "../../components/JogosCard";
 
 const darkTheme = createTheme({
     palette: {
@@ -60,7 +61,14 @@ const Page = () => {
                                         Sobre o site:
                                     </Typography>
                                     <Typography>
-                                        PlayerFinder é um site Gamers brasileiros para Gamers brasileiros!
+                                    Bem-vindo ao PlayerFinder, o seu novo ponto de encontro online para encontrar parceiros de jogo no Brasil!
+                                    Aqui, nós entendemos a importância de encontrar o parceiro de jogo perfeito - alguém que compartilha seu entusiasmo, estratégia e paixão pelos jogos.
+                                    Criado por jogadores brasileiros para jogadores brasileiros, o PlayerFinder é mais do que apenas um site; 
+                                    é uma comunidade vibrante dedicada a criar conexões genuínas entre gamers que buscam experiências multiplayer emocionantes.
+                                    Se você está procurando formar uma equipe para conquistar missões desafiadoras ou simplesmente quer alguém com quem competir em partidas competitivas, o PlayerFinder é o lugar certo para você. Nossa plataforma oferece uma maneira simples e eficaz de encontrar jogadores com interesses semelhantes, seja para aventuras cooperativas, batalhas emocionantes ou partidas descontraídas.
+                                    Aqui, você pode criar seu perfil de jogador, indicar seus jogos favoritos, horários disponíveis e preferências de estilo de jogo. Em seguida, explore nossa comunidade diversificada de jogadores ávidos, pesquisando por títulos específicos ou simplesmente navegando pelas opções disponíveis. Com apenas alguns cliques, você estará conectado a outros gamers entusiasmados que compartilham seus interesses.
+                                    No PlayerFinder, a diversão começa com a conexão. Junte-se a nós e descubra uma maneira totalmente nova de experimentar seus jogos favoritos - com amigos e colegas de equipe que tornam cada jogada ainda mais emocionante. Prepare-se para mergulhar em uma experiência de jogo verdadeiramente colaborativa e faça novas amizades ao longo do caminho.
+                                    Estamos ansiosos para vê-lo no PlayerFinder, onde encontrar seu parceiro de jogo ideal é apenas o começo de uma jornada emocionante. Vamos jogar juntos!
                                     </Typography>
                                 </Grid>
                             </Grid>
@@ -71,41 +79,33 @@ const Page = () => {
                                 display: "flex",
                                 flexDirection: "row",
                                 justifyContent: "center",
-                                width: "80vw"
+                                width: "105%"
                             }}
                         >
                             <Paper
                                 component="div"
                                 style={{
-                                    padding: "1%",
+                                    padding: "0.5%",
                                     margin: "1%",
                                     width: "50%",
                                     backgroundColor: "#202020",
                                     borderRadius: "10px",
                                 }}
                             >
-                                <Grid container spacing={2}>
-                                    <Grid item xs={12}>
-
-                                    </Grid>
-                                </Grid>
+                                
                             </Paper>
 
                             <Paper
                                 component="div"
                                 style={{
-                                    padding: "1%",
+                                    padding: "0.5%",
                                     margin: "1%",
                                     width: "50%",
                                     backgroundColor: "#202020",
                                     borderRadius: "10px",
                                 }}
                             >
-                                <Grid container spacing={2}>
-                                    <Grid item xs={12}>
-
-                                    </Grid>
-                                </Grid>
+                                <JogosAreaCard/>
                             </Paper>
 
                         </div>
