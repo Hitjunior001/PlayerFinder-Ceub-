7576ca9d3b122970cece77fea6131f284d302cd5
Digs-LS <diego.lucas.sousa@live.com>
2024-04-16 18:55:09 -0300

    feat: cria classe para a entidade 'jogo'

---
+++ src/main/java/com/ceub/projetointegradoriii/playerfinder/entity/Jogo.java
@@ -0,0 +1,63 @@
+package com.ceub.projetointegradoriii.playerfinder.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "jogos")
+public class Jogo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "titulo", nullable = false)
+	private String titulo;
+
+	@Column(name = "data_lancamento", nullable = false)
+	private Date dataLancamento;
+
+	public Jogo(int id, String titulo, Date dataLancamento) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.dataLancamento = dataLancamento;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Date getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(Date dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+	@Override
+	public String toString() {
+		return "Jogo [id=" + id + ", titulo=" + titulo + ", dataLancamento=" + dataLancamento + "]";
+	}
+	
+}
