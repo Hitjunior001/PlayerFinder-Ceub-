8ec809d543c8db06f16ee87be551766a1367c67a
Digs-LS <diego.lucas.sousa@live.com>
2024-04-16 18:59:17 -0300

    feat: cria template para o repository da classe 'jogo'

---
+++ src/main/java/com/ceub/projetointegradoriii/playerfinder/repository/JogoRepository.java
@@ -0,0 +1,9 @@
+package com.ceub.projetointegradoriii.playerfinder.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ceub.projetointegradoriii.playerfinder.entity.Jogo;
+
+public interface JogoRepository extends JpaRepository<Jogo, Long> {
+
+}
