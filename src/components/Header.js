38e4dfc840e5c78fb45062ec80a8bb6687ca33f8
Gustavo_Erhardt <119618665+GustavoErhardt@users.noreply.github.com>
2024-04-28 18:55:24 -0300

    [ Header with auth ]

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,45 @@ import { AppBar, Toolbar, Typography, Button, Grid } from "@mui/material";
 import { Link, Routes, Route } from "react-router-dom";
 import MenuTabs from "./MenuTab";
 import UserLogado from "./userLogado";
+import useAuth from "../hooks/useAuth";
 
 export const CustomAppBar = () => {
-  return (
+  const { user } = useAuth();
+  if(user){
+    return (
+      <AppBar position="static">
+        <Toolbar style={{ height: "12vh", padding: "0.5%", backgroundColor: "#0F021C"}} >
+
+          <Grid container spacing={3} style={{ height: "12vh", margin: '0'}}>
+            <Grid item xs style={{textAlign: 'start', paddingLeft: '0'}}>
+              <Link to={"/inicio"}>
+                <img src="logo.png" alt="logo" />
+              </Link>
+            </Grid>
+
+            <Grid item xs={7} style={{alignSelf: 'end'}}>
+              <MenuTabs />
+            </Grid>
+
+            <Grid item xs style={{textAlign: 'center', paddingTop: '0', alignSelf: 'center'}}> 
+              <UserLogado />
+            </Grid>
+
+          </Grid>
+        </Toolbar>
+      </AppBar >
+    );
+  }
+  else return (
     <AppBar position="static">
       <Toolbar style={{ height: "12vh", padding: "0.5%", backgroundColor: "#0F021C"}} >
 
-        <Grid container spacing={3} style={{ height: "12vh", margin: '0'}}>
+        <Grid container spacing={2} style={{ height: "12vh", margin: '0'}}>
           <Grid item xs style={{textAlign: 'start', paddingLeft: '0'}}>
-            <Link to={"/inicio"}>
               <img src="logo.png" alt="logo" />
-            </Link>
           </Grid>
 
-          <Grid item xs={7} style={{alignSelf: 'end'}}>
-            <MenuTabs />
-          </Grid>
-
-          <Grid item xs style={{textAlign: 'center', paddingTop: '0', alignSelf: 'center'}}>
+          <Grid item xs style={{textAlign: 'end', paddingTop: '0', paddingRight: '3vw', alignSelf: 'center'}}>
             <Routes>
               <Route exact path="/cadastro" element={
                 <Typography component="div">
@@ -42,10 +63,6 @@ export const CustomAppBar = () => {
                     </Button>
                   </Link>
                 </Typography>
-              } />
-
-              <Route exact path="/inicio" element={
-                <UserLogado />
               } />
 
             </Routes>
