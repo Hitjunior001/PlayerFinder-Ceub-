91dd453b8561d78926a83478a01f1e036fd19929
Gustavo_Erhardt <119618665+GustavoErhardt@users.noreply.github.com>
2024-04-30 17:13:15 -0300

    [ Toggle editar perfil ]

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -6,27 +6,62 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import TextField from "@mui/material/TextField";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import InputFileUpload from "../../components/fileUpload";
+
 const darkTheme = createTheme({
     palette: {
         mode: "dark",
     },
 });
+  
+  const Page = () => {
+    const [editar, setEditar] = React.useState(false);
+    const toggleEditar = () => {
+      setEditar(!editar);
+    };//Botão editar
 
-const Page = () => {
+    const Itens1 = (
+        <Container>
+            {['Username', 'Nome', 'Email'].map((text) => (
+                <ListItem key={text}>
+                    <ListItemText sx={{ textAlign: 'center', color: '#16C83D', marginRight: '2%' }} primary={text + ":"} />
+                    {!editar ? 
+                        <TextField label={text} />
+                    :
+                        <Typography variant="h6" sx={{ textAlign: 'start' }}>
+                            teste
+                        </Typography>
+                    }
+                </ListItem>
+            ))}
+        </Container>
+      );
+    const Itens2 = (
+        <Container>
+            {['Data de nascimento', 'Estado', 'Gênero'].map((text) => (
+                <ListItem key={text}>
+                    <ListItemText sx={{ textAlign: 'center', color: '#16C83D', marginRight: '2%' }} primary={text + ":"} />
+                    {!editar ? 
+                        <TextField label={text} />
+                    :
+                        <Typography variant="h6" sx={{ textAlign: 'start' }}>
+                            teste
+                        </Typography>
+                    }
+                </ListItem>
+            ))}
+        </Container>
+      );    
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
-        console.log({
-            Usuário: data.get("usuario"),
-            Nome: data.get("nome"),
-            Email: data.get("email"),
-            Senha: data.get("senha"),
-            ConfirmaSenha: data.get("confirmaSenha"),
-            Telefone: data.get("telefone"),
-        });
+        console.log({Usuário: data.get("usuario")});
     };
 
     return (
@@ -48,20 +83,37 @@ const Page = () => {
                         Meu perfil
                     </Typography>
 
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        sx={{
-                            mt: 3,
-                            mb: 2,
-                            width: "8vw",
-                            color: 'white',
-                            bgcolor: "#16C83D",
-                            "&:hover": { backgroundColor: "#16C83D" },
-                        }}
-                    >
-                        Editar
-                    </Button>
+                    {editar ? 
+                        <Button
+                            onClick={toggleEditar}
+                            variant="contained"
+                            sx={{
+                                mt: 3,
+                                mb: 2,
+                                width: "8vw",
+                                color: 'white',
+                                bgcolor: "#16C83D",
+                                "&:hover": { backgroundColor: "#16C83D" },
+                            }}
+                        >
+                            Editar
+                        </Button>
+                        :
+                        <Button
+                            onClick={toggleEditar}
+                            variant="contained"
+                            sx={{
+                                mt: 3,
+                                mb: 2,
+                                width: "8vw",
+                                color: 'white',
+                                bgcolor: "#16C83D",
+                                "&:hover": { backgroundColor: "#16C83D" },
+                            }}
+                        >
+                            Salvar
+                        </Button>
+                    }
 
                     <Box component="form" Validate onSubmit={handleSubmit} sx={{ mt: 1 }}>
                         <div
@@ -88,32 +140,7 @@ const Page = () => {
                                     </Grid>
                                     <Grid item xs={12}>
                                         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                                            <Typography variant="h5" sx={{ color: '#16C83D', marginRight: '2%' }}>
-                                                Usuário:
-                                            </Typography>
-                                            <Typography variant="h6">
-                                                Sempert21
-                                            </Typography>
-                                        </div>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                                            <Typography variant="h5" sx={{ color: '#16C83D', marginRight: '2%' }}>
-                                                Nome:
-                                            </Typography>
-                                            <Typography variant="h6">
-                                                Gustavo Erhardt
-                                            </Typography>
-                                        </div>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                                            <Typography variant="h5" sx={{ color: '#16C83D', marginRight: '2%' }}>
-                                                Email:
-                                            </Typography>
-                                            <Typography variant="h6">
-                                                Gustavoespassos@gmail.com
-                                            </Typography>
+                                            {Itens1}
                                         </div>
                                     </Grid>
                                 </Grid>
@@ -132,82 +159,13 @@ const Page = () => {
                                 <Grid container spacing={5} style={{paddingTop: '20%'}}>
                                     <Grid item xs={12}>
                                         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                                            <Typography variant="h5" sx={{ color: '#16C83D', marginRight: '2%' }}>
-                                                Data de nascimento:
-                                            </Typography>
-                                            <Typography variant="h6">
-                                                07/02/2003
-                                            </Typography>
-                                        </div>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                                            <Typography variant="h5" sx={{ color: '#16C83D', marginRight: '2%' }}>
-                                                Estado:
-                                            </Typography>
-                                            <Typography variant="h6">
-                                                Distrito Federal - DF
-                                            </Typography>
-                                        </div>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                                            <Typography variant="h5" sx={{ color: '#16C83D', marginRight: '2%' }}>
-                                                Gênero:
-                                            </Typography>
-                                            <Typography variant="h6">
-                                                Masculino
-                                            </Typography>
+                                            {Itens2}
                                         </div>
                                     </Grid>
                                 </Grid>
                             </Paper>
 
-                            <Paper
-                                component="div"
-                                style={{
-                                    padding: "1%",
-                                    margin: "1%",
-                                    width: "30vw",
-                                    backgroundColor: "#202020",
-                                    borderRadius: "10px",
-                                }}
-                            >
-                                <Grid container spacing={5} style={{paddingTop: '20%'}}>
-                                <Grid item xs={12}>
-                                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                                            <Typography variant="h5" sx={{ color: '#16C83D', marginRight: '2%' }}>
-                                                Telefone:
-                                            </Typography>
-                                            <Typography variant="h6">
-                                                Não informado *
-                                            </Typography>
-                                        </div>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                                            <Typography variant="h5" sx={{ color: '#16C83D', marginRight: '2%' }}>
-                                                Discord:
-                                            </Typography>
-                                            <Typography variant="h6">
-                                                xXx_ShadowMonstrao_xXx
-                                            </Typography>
-                                        </div>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                                            <Typography variant="h5" sx={{ color: '#16C83D', marginRight: '2%' }}>
-                                                Instagram:
-                                            </Typography>
-                                            <Typography variant="h6">
-                                                Não informado *
-                                            </Typography>
-                                        </div>
-                                    </Grid>
-                                </Grid>
-                            </Paper>
                         </div>
-
                     </Box>
                 </Box>
             </Container>
